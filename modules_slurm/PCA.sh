7dd0fec55d6087270c3038c87cf65f95a38d8571
Wen Luo <wen.luo@nih.gov>
2025-01-29 16:23:29 -0500

    update pca script to accomondate non ensemble vcf

--- modules_slurm/PCA.sh
+++ modules_slurm/PCA.sh
@@ -14,8 +14,14 @@ fi
 
 DIR=$(dirname $VCF)
 NAME=$(basename $VCF .vcf.gz)
+HAS_HC_AF=$(bcftools view -h $VCF | grep HC_AF | wc -l)
 
-bcftools view -f threeCallers,twoCallers -i '(INFO/HC_AF > 0.05) || (INFO/DV_AF > 0.05 )' --types snps $PARAMS -Oz -o ${DIR}/${NAME}.common.eur.vcf.gz $VCF
+if [[ ${HAS_HC_AF} > 0 ]]; then
+	bcftools view -f threeCallers,twoCallers -i '(INFO/HC_AF > 0.05) || (INFO/DV_AF > 0.05 )' --types snps $PARAMS -Oz -o ${DIR}/${NAME}.common.eur.vcf.gz $VCF
+else
+	bcftools view -f 2,3,PASS -i '(INFO/AF > 0.05 )' --types snps $PARAMS -Oz -o ${DIR}/${NAME}.common.eur.vcf.gz $VCF
+fi
+
 tabix -p vcf ${DIR}/${NAME}.common.eur.vcf.gz 
 
 plink2 --vcf ${DIR}/${NAME}.common.eur.vcf.gz  --double-id --allow-extra-chr --set-missing-var-ids @:#\$1,\$2 --indep-pairwise 50 10 0.1 --vcf-half-call m --out ${DIR}/${NAME}
