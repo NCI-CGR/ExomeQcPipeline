7cdc48ee465ffe2187c21cf12db0abc4b6d3b2e5
Wen Luo <wen.luo@nih.gov>
2026-08-10 11:35:02 -0400

    1, remove intermidiate files for space cleanning; 2, bam-matcher not working under R/4.5.1, unload it before running bam-matcher

--- run_snakefile_no_report_slurm.sh
+++ run_snakefile_no_report_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-module load python3/3.10.2 singularity slurm R
+module load python3/3.10.2 singularity
 
 DATE=$(date +%y%m%d)
 proDir=$(awk '($0~/^project:/){print $2}' modules_slurm/config.yaml | sed "s/['\"]//g")
@@ -10,14 +10,14 @@ mkdir -p ${proDir}/logs_${DATE}
 snakemake --unlock -s Snakefile_no_report_slurm --configfile modules_slurm/config.yaml
 
 #sbcmd="qsub -cwd -q {cluster.q} -pe by_node {threads} -o logs_${DATE}/ -e logs_${DATE}/ -V"
-sbcmd="sbatch --time=24:00:00 --mem=64g --partition=bigmemq,cgrq --cpus-per-task={threads} --output=${proDir}/logs_${DATE}/snakejob_%j.out"
+sbcmd="sbatch --time=24:00:00 --mem=96g --partition=bigmemq,cgrq --cpus-per-task={threads} --output=${proDir}/logs_${DATE}/snakejob_%j.out"
 
 #qsub -cwd -q seq-calling.q -N run_Snakefile_no_report  -o logs_${DATE}/Snakefile_no_report.stdout -e logs_${DATE}/Snakefile_no_report.stderr -b y "module load python3 sge R/3.4.0 gcc zlib;snakemake -pr -s Snakefile_no_report --keep-going --rerun-incomplete --local-cores 1 --jobs 1000 --configfile modules/config.yaml --cluster \"$sbcmd\" --cluster-config cluster.yaml --latency-wait 120 all"
 
 #echo "PACKAGE_VERSION={{version}}"
-echo "#!/bin/sh" > ${proDir}/logs_${DATE}/run_snakefile_no_report_slurm.sbatch
+echo "#!/bin/bash" > ${proDir}/logs_${DATE}/run_snakefile_no_report_slurm.sbatch
 
-echo "module load python3/3.10.2 singularity slurm R" >> ${proDir}/logs_${DATE}/run_snakefile_no_report_slurm.sbatch
+echo ". /etc/profile.d/modules.sh; module load python3/3.10.2 singularity" >> ${proDir}/logs_${DATE}/run_snakefile_no_report_slurm.sbatch
 echo "python print_version.py" >> ${proDir}/logs_${DATE}/run_snakefile_no_report_slurm.sbatch
 echo "snakemake -pr -s Snakefile_no_report_slurm --use-singularity --singularity-args \"--bind /DCEG,/scratch\" --keep-going --rerun-incomplete --local-cores 1 --jobs 1000 --configfile modules_slurm/config.yaml --cluster \"$sbcmd\" --cluster-config cluster_slurm.yaml --latency-wait 120" >> ${proDir}/logs_${DATE}/run_snakefile_no_report_slurm.sbatch
 
